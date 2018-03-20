faf180a23e8857104cd7712032ee8333bef85844
Andre Perkins <frodre@gmail.com>
2018-03-20 15:10:31 -0700

    Remove EBM from Application Page

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-import numpy as np
-from bokeh.plotting import figure, curdoc, show
-from bokeh.layouts import layout, WidgetBox
-from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Div, Slider, TextInput, Select, Button
-from bokeh.models.tools import HoverTool
-
-# import holoviews as hv
-# import holoviews.plotting.bokeh
-# import geoviews as gv
-# import geoviews.feature as gf
-# import cartopy.crs as ccrs
+from bokeh.plotting import curdoc
+from bokeh.layouts import layout
 
 import terrasol
 import climate
 
-# hv.extension('bokeh')
-# renderer = hv.renderer('bokeh')
-# hv.opts("Overlay [width=600 height=500] Image (cmap='viridis') Feature (line_color='black')")
-# hv.output(size=200)
 
 # Initialize Star and Planet
 terrasol_ux = terrasol.TerraSol()
 
-# Initialize Climate Model
-energy_in = terrasol_ux
-climate_ux = climate.PlanetClimateEBM(terrasol_ux)
-print(climate_ux.climate_result)
+# # Initialize Climate Model
+# energy_in = terrasol_ux
+# climate_ux = climate.PlanetClimateEBM(terrasol_ux)
+# print(climate_ux.climate_result)
 
 # Initialize Simpler Climate Model
 simple_climate_ux = climate.SimpleClimate(terrasol_ux)
@@ -34,25 +20,12 @@ plot_layout = layout([[terrasol_ux.plot],
                       terrasol_ux.div_row,
                       terrasol_ux.sliders,
                       [simple_climate_ux.plot],
-                      climate_ux.input_wx,
-                      [climate_ux.calc_button]],
+                      # climate_ux.input_wx,
+                      # [climate_ux.calc_button]
+                      ],
                      sizing_mode='fixed')
 # show(plot_layout)
 curdoc().add_root(plot_layout)
 curdoc().title = 'TerraSol'
 
-# xr_dataset = gv.Dataset(data=final_T_dataframe.to_dataset(name='T'),
-#                         vdims=['T'],
-#                         kdims=['lat', 'lon'],
-#                         crs=ccrs.PlateCarree())
-# temp_map = xr_dataset.to.image()
-# playout = temp_map * gf.coastline()
-#
-# points = hv.Points(np.random.randn(1000,2 )).opts(plot=dict(tools=['box_select', 'lasso_select']))
-# points2 = hv.Points(np.random.randn(1000,2 )).opts(plot=dict(tools=['box_select', 'lasso_select']))
-# playout = points + points2
-# doc = renderer.server_doc(playout)
-# doc.title = 'Testing'
-#
-# # plot = renderer.get_plot(temp_map, curdoc())
-# # show(layout([plot.state]))
+
--- scratch.py
+++ scratch.py
@@ -2,6 +2,34 @@ import numpy as np
 import holoviews as hv
 import holoviews.plotting.bokeh
 
+# import holoviews as hv
+# import holoviews.plotting.bokeh
+# import geoviews as gv
+# import geoviews.feature as gf
+# import cartopy.crs as ccrs
+
+
+# hv.extension('bokeh')
+# renderer = hv.renderer('bokeh')
+# hv.opts("Overlay [width=600 height=500] Image (cmap='viridis') Feature (line_color='black')")
+# hv.output(size=200)
+
+# xr_dataset = gv.Dataset(data=final_T_dataframe.to_dataset(name='T'),
+#                         vdims=['T'],
+#                         kdims=['lat', 'lon'],
+#                         crs=ccrs.PlateCarree())
+# temp_map = xr_dataset.to.image()
+# playout = temp_map * gf.coastline()
+#
+# points = hv.Points(np.random.randn(1000,2 )).opts(plot=dict(tools=['box_select', 'lasso_select']))
+# points2 = hv.Points(np.random.randn(1000,2 )).opts(plot=dict(tools=['box_select', 'lasso_select']))
+# playout = points + points2
+# doc = renderer.server_doc(playout)
+# doc.title = 'Testing'
+#
+# # plot = renderer.get_plot(temp_map, curdoc())
+# # show(layout([plot.state]))
+
 renderer = hv.renderer('bokeh')
 
 points = hv.Points(np.random.randn(1000,2 )).opts(plot=dict(tools=['box_select', 'lasso_select']))
